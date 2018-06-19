3942641a5f89acd45fa6f02fb8af621103238b0c
Chris Froehlingsdorf <chris@5cf.de>
2018-06-19 20:59:21 +0200

    auto-generated-commit

--- example/example.js
+++ example/example.js
@@ -1,41 +1,10 @@
 "use strict";
 
+const {kafka} = require("./config.js");
 const Sarkac = require("./../index.js");
 
 const config = {
-    kafka: {
-        noptions: {
-            "metadata.broker.list": "localhost:9092",
-            "group.id": "sarkac-example-group",
-            "client.id": "sarkac-example-id",
-            "event_cb": true,
-            "compression.codec": "snappy",
-            "api.version.request": true,
-            "socket.keepalive.enable": true,
-            "socket.blocking.max.ms": 100,
-            "enable.auto.commit": false,
-            "auto.commit.interval.ms": 100,
-            "heartbeat.interval.ms": 250,
-            "retry.backoff.ms": 250,
-            "fetch.min.bytes": 100,
-            "fetch.message.max.bytes": 2 * 1024 * 1024,
-            "queued.min.messages": 100,
-            "fetch.error.backoff.ms": 100,
-            "queued.max.messages.kbytes": 50,
-            "fetch.wait.max.ms": 1000,
-            "queue.buffering.max.ms": 1000,
-            "batch.num.messages": 10000
-        },
-        tconf: {
-            "auto.offset.reset": "earliest",
-            "request.required.acks": 1
-        },
-        batchOptions: {
-            batchSize: 5,
-            commitEveryNBatch: 1,
-            concurrency: 1
-        }
-    },
+    kafka,
     mongo: {
         url: "mongodb://localhost:27017/sarkac_example",
         options: {
@@ -65,7 +34,7 @@ const config = {
     target: {
         produceAnomalies: true,
         topic: "sarkac-detected-anomalies",
-        partitions: 30
+        partitions: 1
     },
     hooks: {
         beforeMessageProcessing: (message, callback) => { callback(null, message); },
@@ -75,9 +44,9 @@ const config = {
 
 const sarkac = new Sarkac(config);
 
-sarkac.on("anomaly", (anomaly, message) => console.log(anomaly, message));
-sarkac.on("message", (message) => {/* empty */});
+sarkac.on("anomaly", (anomaly, message) => console.log(anomaly));
+sarkac.on("message", (message) => console.log(message));
 sarkac.on("error", (error) => console.error(error));
 
 sarkac.analyse().catch((error) => console.error(error));
-setTimeout(sarkac.close.bind(sarkac), 25000);
+//setTimeout(sarkac.close.bind(sarkac), 25000);
